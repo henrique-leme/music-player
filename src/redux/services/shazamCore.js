ba9f68be85b1d8a8974d3bc8977885af799d01d6
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-10-07 19:38:56 -0300

    ✨feat: create shazam core

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,30 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     'X-RapidAPI-Key': 'cd2fa4c330mshbae98045be8bca9p1de4adjsn5174c75d389d',
+//     'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com',
+//   },
+// };
+
+// fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
+//   .then((response) => response.json())
+//   .then((response) => console.log(response))
+//   .catch((err) => console.error(err));
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', 'cd2fa4c330mshbae98045be8bca9p1de4adjsn5174c75d389d');
+      return headers;
+    },
+  }),
+  endpoints: (builders) => ({
+    getTopCharts: builders.query({ query: () => '/charts/world' }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
