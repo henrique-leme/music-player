9dc5db1985409ab58c524702ce9ef3486b00b452
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-10-07 19:44:38 -0300

    ✨feat: fetching data from api

---
+++ src/pages/Discover.jsx
@@ -0,0 +1,39 @@
+import { Error, Loader, SongCard } from '../components';
+import { musicGenres } from '../assets/constants';
+import { useGetTopChartsQuery } from '../redux/services/shazamCore';
+
+const Discover = () => {
+  const { data, isFetching, error } = useGetTopChartsQuery();
+  const genreTitle = 'Pop';
+
+  if (isFetching) return <Loader title="Loading songs..." />;
+  if (error) return <Error />;
+
+  return (
+    <div className="flex flex-col">
+      <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+        <h2 className="font-bold text-3x1 text-white text-left">
+          Discover {genreTitle}
+        </h2>
+        <select
+          onChange={() => {}}
+          value=""
+          className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
+        >
+          {musicGenres.map((musicGenre) => (
+            <option key={musicGenre.value} value={musicGenre.value}>
+              {musicGenre.title}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {data?.map((song, index) => (
+          <SongCard key={song.key} song={song} i={index} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Discover;
