df7022088e44b15449cb224a42c8e3661b78d3c2
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-10-10 23:14:28 -0300

    ✨feat: cards style

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -4,12 +4,16 @@ import { useDispatch } from 'react-redux';
 import PlayPause from './PlayPause';
 import { playPause, setActiveSongs } from '../redux/features/playerSlice';
 
-const SongCard = ({ songs, index }) => (
-  <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
-    <div className="relative w-full h-56 group">
-      <div className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${activeSong?.title === songs.title ? 'flex bg-black bg-opacity-70' : 'hidden'}`} />
+const SongCard = ({ song, index }) => {
+  const activeSong = 'Test';
+  return (
+
+    <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
+      <div className="relative w-full h-56 group">
+        <div className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${activeSong?.title === song.title ? 'flex bg-black bg-opacity-70' : 'hidden'}`} />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default SongCard;
