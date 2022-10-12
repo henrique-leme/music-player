d68e3b18e791be22dfa8cb11e2ee3d8cf97d7fff
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-10-11 22:53:08 -0300

    ➕refactor: update songcard

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -4,8 +4,10 @@ import { useDispatch } from 'react-redux';
 import PlayPause from './PlayPause';
 import { playPause, setActiveSongs } from '../redux/features/playerSlice';
 
-const SongCard = ({ song, index }) => {
-  const activeSong = 'test';
+const SongCard = ({ song, isPlaying, activeSong, index, data }) => {
+  const handlePauseClick = () => {};
+  const handlePlayClick = () => {};
+
   return (
     <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
       <div className="relative w-full h-56 group">
@@ -15,7 +17,13 @@ const SongCard = ({ song, index }) => {
           .title ? 'flex bg-black bg-opacity-70' : 'hidden'}
         `}
         >
-          <PlayPause />
+          <PlayPause
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            song={song}
+            handlePause={handlePauseClick}
+            handlePlay={handlePlayClick}
+          />
         </div>
         <img src={song.imgages?.coverart} alt="song_img" />
       </div>
