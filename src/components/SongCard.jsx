82c16ca7a4bc3704c61cacff27d32620b42a49dc
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-10-12 21:28:00 -0300

    ✨feat: play button dispatch

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -5,8 +5,16 @@ import PlayPause from './PlayPause';
 import { playPause, setActiveSongs } from '../redux/features/playerSlice';
 
 const SongCard = ({ song, isPlaying, activeSong, index, data }) => {
-  const handlePauseClick = () => {};
-  const handlePlayClick = () => {};
+  const dispatch = useDispatch();
+
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
+
+  const handlePlayClick = () => {
+    dispatch(setActiveSongs({ song, data, index }));
+    dispatch(playPause(true));
+  };
 
   return (
     <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
