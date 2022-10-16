f0cce479aa1fc5c615fe398ef6a50c1381ed3f50
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-10-16 19:05:04 -0300

    ✨feat: add play pause function

--- src/components/SongBar.jsx
+++ src/components/SongBar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import PlayPause from './PlayPause';
+
 const SongBar = ({ song, i, artistId, isPlaying, activeSong, handlePauseClick, handlePlayClick }) => (
   <div className={`w-full flex flex-row items-center hover:bg-[#4c426e] ${activeSong?.title === song?.title ? 'bg-[#4c426e]' : 'bg-transparent'} py-2 p-4 rounded-lg cursor-pointer mb-2`}>
     <h3 className="font-bold text-base text-white mr-3">{i + 1}.</h3>
@@ -9,6 +10,7 @@ const SongBar = ({ song, i, artistId, isPlaying, activeSong, handlePauseClick, h
       <img
         className="w-20 h-20 rounded-lg"
         src={artistId ? song?.attributes?.artwork?.url.replace('{w}', '125').replace('{h}', '125') : song?.images?.coverart}
+        alt={song?.title}
       />
       <div className="flex-1 flex flex-col justify-center mx-3">
         {!artistId ? (
@@ -27,6 +29,18 @@ const SongBar = ({ song, i, artistId, isPlaying, activeSong, handlePauseClick, h
         </p>
       </div>
     </div>
+    {!artistId
+      ? (
+        <PlayPause
+          isPlaying={isPlaying}
+          activeSong={activeSong}
+          song={song}
+          handlePause={handlePauseClick}
+          handlePlay={() => handlePlayClick(song, i)}
+        />
+      )
+      : null}
+  </div>
 );
 
 export default SongBar;
