2e05430be43abf4fa45acdea16df07f90d602566
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-10-16 19:03:34 -0300

    ✨feat: create songbar

---
+++ src/components/SongBar.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import PlayPause from './PlayPause';
+const SongBar = ({ song, i, artistId, isPlaying, activeSong, handlePauseClick, handlePlayClick }) => (
+);
+
+export default SongBar;
