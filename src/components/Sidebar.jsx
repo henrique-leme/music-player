274e3c611a4a6751e1706ef6a2912c94c29835a4
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-10-17 23:51:35 -0300

    ➕refactor: update sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,6 +6,12 @@ import { links } from '../assets/constants';
 
 const Sidebar = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    <div className="md:flex hidden flex-col w=[240px] py-10 px-4 bg-[#191624]">
+      <img src={logo} alt={logo} className="w-full h-14 object-contain" />
+    </div>
+  );
 };
 
 export default Sidebar;
