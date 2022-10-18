cbf0a3e8462696df192b63e3fd56f5e82440f617
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-10-17 23:51:19 -0300

    ➕refactor: update sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,11 @@
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { RiCloseLine } from 'react-icons/ri';
+import { logo } from '../assets';
+import { links } from '../assets/constants';
 
-const Sidebar = () => (
-  <div>Sidebar</div>
-);
+const Sidebar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+};
 
 export default Sidebar;
