8f84ccb1cb55fd38ac5986cbc09c52699ce553ea
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-10-19 23:55:37 -0300

    ➕refactor: update navlink in sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { RiCloseLine } from 'react-icons/ri';
+import { HiOutlineMenu } from 'react-icons/hi';
 import { logo } from '../assets';
 import { links } from '../assets/constants';
 
@@ -24,10 +25,17 @@ const Sidebar = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <div className="md:flex hidden flex-col w=[240px] py-10 px-4 bg-[#191624]">
-      <img src={logo} alt={logo} className="w-full h-14 object-contain" />
-      <NavLinks />
-    </div>
+    <>
+      <div className="md:flex hidden flex-col w=[240px] py-10 px-4 bg-[#191624]">
+        <img src={logo} alt={logo} className="w-full h-14 object-contain" />
+        <NavLinks />
+      </div>
+      <div className="absolute md:hidden block top-6 right-3">
+        {mobileMenuOpen ? (
+          <RiCloseLine />
+        ) : <HiOutlineMenu />}
+      </div>
+    </>
   );
 };
 
