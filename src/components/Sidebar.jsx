8a21874251ecdff9a2f79d4916f9a016049b4448
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-10-19 23:45:14 -0300

    ➕refactor: update navlink in sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,14 +4,16 @@ import { RiCloseLine } from 'react-icons/ri';
 import { logo } from '../assets';
 import { links } from '../assets/constants';
 
-const NavLinks = () => (
+const NavLinks = ({ handleClick }) => (
   <div className="mt-10">
     {links.map((item) => (
       <NavLink
         key={item.name}
-        to={item.path}
+        to={item.to}
         className="flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-purple-400"
+        onClick={() => handleClick && handleClick()}
       >
+        <item.icon className="w-6 h-6 mr-2" />
         {item.name}
       </NavLink>
     ))}
@@ -24,9 +26,10 @@ const Sidebar = () => {
   return (
     <div className="md:flex hidden flex-col w=[240px] py-10 px-4 bg-[#191624]">
       <img src={logo} alt={logo} className="w-full h-14 object-contain" />
-      <NavLink />
+      <NavLinks />
     </div>
   );
 };
 
 export default Sidebar;
+
