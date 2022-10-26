9baf1071c527eaf1d763675bd0bdf6dae0d50f51
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-10-25 23:35:52 -0300

    ✨feat: topcharts style

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -32,9 +32,9 @@ const TopPlay = () => {
     <div ref={divRef} className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col">
       <div className="w-full flex flex-col">
         <div className="flex flex-row justify-between items-center">
-          <h2>Top Charts</h2>
+          <h2 className="text-white font-bold text-2x1">Top Charts</h2>
           <Link to="/top-charts">
-            <p>See more...</p>
+            <p className="text-gray-300 text-base cursor-pointer">See more...</p>
           </Link>
         </div>
       </div>
