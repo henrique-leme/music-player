c5f00866baba64f7d0a15b9bafcab54ed95b2d91
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-11-07 23:51:11 -0300

    ➕refactor: topchart

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -53,6 +53,17 @@ const TopPlay = () => {
           ))}
         </div>
       </div>
+    <div className='flex flex-row justify-between'>
+      <div ref={divRef} className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col">
+      <div className="w-full flex flex-col">
+        <div className="flex flex-row justify-between items-center">
+          <h2 className="text-white font-bold text-2x1">Top Charts</h2>
+          <Link to="/top-charts">
+            <p className="text-gray-300 text-base cursor-pointer">See more...</p>
+          </Link>
+        </div>
+    </div>
+      </div>
     </div>
   );
 };
