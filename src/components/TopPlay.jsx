1c9d0f90f513be89501a54843a3091b3b96c3bfc
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-11-09 23:09:18 -0300

    ➕refactor: update imports

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -1,7 +1,12 @@
 import { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { Swiper, SwiperSlide } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { FreeMode } from 'swiper';
+import { useGetTopChartsQuery } from '../redux/services/shazamCore';
+
+import PlayPause from './PlayPause';
+import { playPause, setActiveSong } from '../redux/features/playerSlice';
 import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
 import 'swiper/css';
