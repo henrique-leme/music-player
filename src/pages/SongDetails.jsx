4bfa770e8d2fffd8256e41b016a57048743fdc7c
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-11-19 18:08:05 -0300

    ➕refactor: update imports

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -9,9 +9,8 @@ const SongDetails = () => {
   const { songid } = useParams();
   const dispatch = useDispatch();
   const { activeSong, isPlaying } = useSelector((state) => state.player);
-  const { data: songData, isFetching: isFetchingSongDetails} = useGeSongsDetailsQuery({songid})
+  const { data: songData, isFetching: isFetchingSongDetails } = useGeSongsDetailsQuery({ songid });
   console.log(songid);
-
 
   return (
     <div className="flex felx-col">
@@ -19,10 +18,10 @@ const SongDetails = () => {
 
       <div className="mb-10">
         <h2 className="text-white text-3x1 font-bold">Lyrics</h2>
-        <div className='mt-5'>
-    {songData?.sections[1].type === 'LYRICS' ? songData?.sections[1].text.map((line, i) => (
-      <p>{line}</p>
-    ))}
+        <div className="mt-5">
+          {songData?.sections[1].type === 'LYRICS' ? songData?.sections[1].text.map((line, i) => (
+            <p className="text-gray-400 text-base my-1">{line}</p>
+          )) : <p className="text-gray-400 text-base my-1">No lyrics found.</p>}
         </div>
       </div>
     </div>
