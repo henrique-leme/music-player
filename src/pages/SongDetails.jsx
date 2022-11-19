624e3e4ad0ccae2a14c3faf1ae88a1559d303e31
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-11-18 23:49:36 -0300

    ➕refactor: update songdetails

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -17,7 +17,11 @@ const SongDetails = () => {
 
       <div className="mb-10">
         <h2 className="text-white text-3x1 font-bold">Lyrics</h2>
-        <div className="mt-5" />
+        <div className='mt-5'>
+    {songData?.sections[1].type === 'LYRICS' ? songData?.sections[1].text.map((line, i) => (
+      <p>{line}</p>
+    ))}
+        </div>
       </div>
     </div>
   );
