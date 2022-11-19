8ad2fd1df6482c210fac5af92a7f952ebb190b87
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-11-19 18:14:51 -0300

    ✨feat: create detailsHeader

---
+++ src/components/DetailsHeader.jsx
@@ -0,0 +1,5 @@
+const DetailsHeader = () => (
+  
+);
+
+export default DetailsHeader;
--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -14,7 +14,7 @@ const SongDetails = () => {
 
   return (
     <div className="flex felx-col">
-      <DetailsHeader artistId={artistId} songData={songData} />
+      <DetailsHeader artistId="" songData={songData} />
 
       <div className="mb-10">
         <h2 className="text-white text-3x1 font-bold">Lyrics</h2>
