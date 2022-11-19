3b1e039e387e385872ced6fb026aa14d5ceaaaf1
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-11-19 18:22:28 -0300

    ➕refactor: update detailsheader

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -1,5 +1,12 @@
-const DetailsHeader = () => (
-  
+import { Link } from 'react-router-dom';
+
+const DetailsHeader = ({ artistId, artistData, songData }) => (
+  <div className="relative w-full flex flex-col">
+    <div className="w-full bg-gradient-to-l from-transparent to-black sm:h-48 h-28" />
+    <div className="absolute inset-0 f;ex items-center">
+      <img src="" alt="" />
+    </div>
+  </div>
 );
 
 export default DetailsHeader;
