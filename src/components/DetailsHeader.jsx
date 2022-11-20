e40dd95cffda6ec7c8af2698edb0851ee6662e38
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-11-19 21:26:39 -0300

    ➕refactor: add img src

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -4,7 +4,7 @@ const DetailsHeader = ({ artistId, artistData, songData }) => (
   <div className="relative w-full flex flex-col">
     <div className="w-full bg-gradient-to-l from-transparent to-black sm:h-48 h-28" />
     <div className="absolute inset-0 f;ex items-center">
-      <img src="" alt="" />
+      <img src={artistId ? artistData?.artists[artistId].attributes?.arwork?.url.replace('{w}', '500').replace('{h}', '500') : songData?.images?.coverart} alt="Song Artwork" />
     </div>
   </div>
 );
