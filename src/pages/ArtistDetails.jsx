766405a7605d61fdf8bcd51977a720cba345519b
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-11-22 08:50:53 -0300

    ➕refactor: update artistdetails

--- src/pages/ArtistDetails.jsx
+++ src/pages/ArtistDetails.jsx
@@ -4,15 +4,14 @@ import { useSelector, useDispatch } from 'react-redux';
 import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
 
 import { setActiveSong, playPause } from '../redux/features/playerSlice';
-import { useGetSongDetailsQuery, useGetSongRelatedQuery } from '../redux/services/shazamCore';
+import { useGetArtistDetailsQuery } from '../redux/services/shazamCore';
 
 const ArtistDetails = () => {
   const dispatch = useDispatch();
-  const { songid, id: artistId } = useParams();
+  const { id: artistId } = useParams();
   const { activeSong, isPlaying } = useSelector((state) => state.player);
 
-  const { data, isFetching: isFetchinRelatedSongs, error } = useGetSongRelatedQuery({ songid });
-  const { data: songData, isFetching: isFetchingSongDetails } = useGetSongDetailsQuery({ songid });
+  const { data: artistData, isFetching: isFetchingArtistDetails } = useGetArtistDetailsQuery({ artistId });
 
   if (isFetchingSongDetails && isFetchinRelatedSongs) return <Loader title="Searching for song details..." />;
   if (error) return <Error />;
