c6f8e1ec277673598edf876dbe5e419ced58e94f
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-11-22 08:57:50 -0300

    ➕refactor: update artistdetails

--- src/pages/ArtistDetails.jsx
+++ src/pages/ArtistDetails.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
 
-import { setActiveSong, playPause } from '../redux/features/playerSlice';
 import { useGetArtistDetailsQuery } from '../redux/services/shazamCore';
 
 const ArtistDetails = () => {
-  const dispatch = useDispatch();
   const { id: artistId } = useParams();
   const { activeSong, isPlaying } = useSelector((state) => state.player);
 
