a5c2359df8700e4641409032184a74dd27bc3ba0
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-11-22 08:51:17 -0300

    ➕refactor: update artistdetails

--- src/pages/ArtistDetails.jsx
+++ src/pages/ArtistDetails.jsx
@@ -16,15 +16,6 @@ const ArtistDetails = () => {
   if (isFetchingSongDetails && isFetchinRelatedSongs) return <Loader title="Searching for song details..." />;
   if (error) return <Error />;
 
-  const handlePauseClick = () => {
-    dispatch(playPause(false));
-  };
-
-  const handlePlayClick = (song, i) => {
-    dispatch(setActiveSong({ song, data, i }));
-    dispatch(playPause(true));
-  };
-
   return (
     <div className="flex flex-col">
       <DetailsHeader
