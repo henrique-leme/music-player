e09e124d9fb83d0e16384855cb62e4161316e78d
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-11-22 03:58:09 -0300

    ➕refactor: add playpause

--- src/pages/ArtistDetails.jsx
+++ src/pages/ArtistDetails.jsx
@@ -20,6 +20,15 @@ const ArtistDetails = () => {
   if (isFetchingSongDetails && isFetchinRelatedSongs) return <Loader title="Searching for song details..." />;
   if (error) return <Error />;
 
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
+
+  const handlePlayClick = (song, i) => {
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
+  };
+
   return (
   );
 };
