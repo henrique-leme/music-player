4cc2e6bb6591faaef1a51ce3bdc0e6e2d951b325
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-11-22 03:58:54 -0300

    ➕refactor: add detailsheader

--- src/pages/ArtistDetails.jsx
+++ src/pages/ArtistDetails.jsx
@@ -1,6 +1,3 @@
-const ArtistDetails = () => (
-  <div>ArtistDetails</div>
-);
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
@@ -30,6 +27,27 @@ const ArtistDetails = () => {
   };
 
   return (
+    <div className="flex flex-col">
+      <DetailsHeader
+        artistId={artistId}
+        songData={songData}
+      />
+
+      <div className="mb-10">
+        <h2 className="text-white text-3xl font-bold">Lyrics</h2>
+
+        <div className="mt-5">
+          {songData?.sections[1].type === 'LYRICS' ? songData?.sections[1]?.text.map((line, i) => (
+            <p key={`lyrics-${line}-${i}`} className="text-gray-400 text-base my-1">{line}</p>
+          ))
+            : (
+              <p className="text-gray-400 text-base my-1">Sorry, No lyrics found!</p>
+            )}
+        </div>
+      </div>
+
+
+    </div>
   );
 };
 
