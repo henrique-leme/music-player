5d3dfa17d86d1eb7290d4480d0324362fb8dc694
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-11-22 10:46:13 -0300

    ➕refactor: update around you

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
+import { Error, Loader, SongCard } from '../components';
 
-const CountryTracks = () => <div>CountryTracks</div>;
+const CountryTracks = () => <div />;
 
 export default CountryTracks;
