6337cf795a02f3bace43dd4f3c1d73540dc03523
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-11-22 21:35:18 -0300

    ➕refactor: update core

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,18 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     'X-RapidAPI-Key': 'cd2fa4c330mshbae98045be8bca9p1de4adjsn5174c75d389d',
-//     'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com',
-//   },
-// };
-
-// fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
-//   .then((response) => response.json())
-//   .then((response) => console.log(response))
-//   .catch((err) => console.error(err));
-
 export const shazamCoreApi = createApi({
   reducerPath: 'shazamCoreApi',
   baseQuery: fetchBaseQuery({
@@ -22,11 +9,6 @@ export const shazamCoreApi = createApi({
       return headers;
     },
   }),
-  endpoints: (builders) => ({
-    getTopCharts: builders.query({ query: () => '/charts/world' }),
-    getSongDetails: builders.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
-    getSongRelated: builders.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
-    getArtistDetails: builders.query({ query: ({ artistId }) => `/artists/details?artist_id${artistId}` }),
   endpoints: (builder) => ({
     getTopCharts: builder.query({ query: () => '/charts/world' }),
     getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` }),
@@ -42,5 +24,4 @@ export const {
   useGetArtistDetailsQuery,
   useGetSongDetailsQuery,
   useGetSongRelatedQuery,
-  useGetArtistDetailsQuery,
 } = shazamCoreApi;
