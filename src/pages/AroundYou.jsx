c6d49ed0cb3ecc33bc1b3e997816f16d392982a0
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-11-22 21:31:42 -0300

    ✨feat: geo api working

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -3,15 +3,23 @@ import { useSelector } from 'react-redux';
 import axios from 'axios';
 import { Error, Loader, SongCard } from '../components';
 
-const AroundYou = () =>{
-const [country, setCountry] = useState('');
-const [loading, setLoading] = useState(true);
-const {activeSong, isPlaying} = useSelector((state) => state.player);
+const AroundYou = () => {
+  const [country, setCountry] = useState('');
+  const [loading, setLoading] = useState(true);
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
 
+  console.log(country);
 
-    return (
-        
-    )};
+  useEffect(() => {
+    axios.get('https://geo.ipify.org/api/v2/country?apiKey=at_geMgqdzDsveiYESi8JEhqgPwTbtfE')
+      .then((res) => setCountry(res?.data?.location?.country))
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false));
+  }, [country]);
 
+  return (
+    <div />
+  );
+};
 
 export default AroundYou;
