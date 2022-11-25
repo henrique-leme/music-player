e862a6b66f13da26a6a0209e89dc8589dd785a61
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-11-25 10:36:35 -0300

    ➕refactor: add conts

--- src/pages/TopCharts.jsx
+++ src/pages/TopCharts.jsx
@@ -3,6 +3,8 @@ import { Error, Loader, SongCard } from '../components';
 import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
 const TopCharts = () => {
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data, isFetching, error } = useGetTopChartsQuery();
 };
 
 export default TopCharts;
