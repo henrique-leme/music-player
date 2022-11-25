a0e07c61943f2e5ef415daa140b7b0270b64bb92
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-11-25 08:12:15 -0300

    ➕refactor:  add data query

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import axios from 'axios';
 import { Error, Loader, SongCard } from '../components';
+import { useGetSongsByCountryQuery } from '../redux/services/shazamCore';
 
 const AroundYou = () => {
   const [country, setCountry] = useState('');
   const [loading, setLoading] = useState(true);
   const { activeSong, isPlaying } = useSelector((state) => state.player);
-
-  console.log(country);
+  const { data, isFetching, error } = useGetSongsByCountryQuery(country);
 
   useEffect(() => {
     axios.get('https://geo.ipify.org/api/v2/country?apiKey=at_geMgqdzDsveiYESi8JEhqgPwTbtfE')
@@ -16,6 +16,9 @@ const AroundYou = () => {
       .catch((err) => console.log(err))
       .finally(() => setLoading(false));
   }, [country]);
+
+  if (isFetching && loading) return <Loader title="Loading songs around you..." />;
+  if (error && country) return <Error />;
 
   return (
     <div />
