0b94d3d42abf3cfe95a49250cc2825e5cbb0311d
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-11-25 10:56:24 -0300

    ➕refactor: add content

--- src/pages/TopArtists.jsx
+++ src/pages/TopArtists.jsx
@@ -6,6 +6,22 @@ const TopArtists = () => {
 
   if (isFetching) return <Loader title="Loading songs around you..." />;
   if (error) return <Error />;
+  return (
+    <div className="flex flex-col">
+      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
+        Top Artists from the Moment!!
+      </h2>
+      <div className="flex flex-wrap sm:justify-startjustify-center gap-8">
+        {data?.map((track) => (
+          <ArtistCard
+            key={track.key}
+            track={track}
+          />
+        ))}
+      </div>
+    </div>
+
+  );
 };
 
 export default TopArtists;
