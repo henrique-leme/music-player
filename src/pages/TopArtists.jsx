36a9b04c01891145fe5c018fd3ef45afb059bf5e
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-11-25 10:56:13 -0300

    ➕refactor: add consts

--- src/pages/TopArtists.jsx
+++ src/pages/TopArtists.jsx
@@ -2,6 +2,10 @@ import { Error, Loader, ArtistCard } from '../components';
 import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
 const TopArtists = () => {
+  const { data, isFetching, error } = useGetTopChartsQuery();
+
+  if (isFetching) return <Loader title="Loading songs around you..." />;
+  if (error) return <Error />;
 };
 
 export default TopArtists;
