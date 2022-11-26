3d219d61fe70b2e41c24b25b99fb24e5dd050097
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-11-26 06:45:57 -0300

    ➕refactor: add imports

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,5 +1,8 @@
-const Searchbar = () => (
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { FiSearch } from 'react-icons/fi';
 
+const Searchbar = () => (
   <form
     autoComplete="off"
     action=""
