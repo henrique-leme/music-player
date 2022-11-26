66787e804939c34db312ee0fb31265dd6b6241ff
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-11-26 06:42:54 -0300

    ➕refactor: add content

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,5 +1,13 @@
 const Searchbar = () => (
-  <div>Loader</div>
+
+  <form
+    autoComplete="off"
+    action=""
+    className="p-2 text-gray-400 focus-within:text-gray-600"
+  >
+    <label htmlFor="search-field" className="sr-only">Search for songs</label>
+    <div className="flex flex-row justify-start items-center" />
+  </form>
 );
 
 export default Searchbar;
