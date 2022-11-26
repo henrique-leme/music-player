b33aa3373fcac510638de270b1fa3ad63fd10961
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-11-26 07:03:36 -0300

    ➕refactor: add input

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -9,7 +9,19 @@ const Searchbar = () => (
     className="p-2 text-gray-400 focus-within:text-gray-600"
   >
     <label htmlFor="search-field" className="sr-only">Search for songs</label>
-    <div className="flex flex-row justify-start items-center" />
+    <div className="flex flex-row justify-start items-center">
+      <FiSearch className="w-5 h-5 ml-4" />
+      <input
+        type="search"
+        name="search-field"
+        autoComplete="off"
+        id="search-field"
+        placeholder="Search"
+        value=""
+        onChange={() => {}}
+        className="flex-1 bg-transparent border-none outline-none placeholder-gray-500 text-base text-white p-4"
+      />
+    </div>
   </form>
 );
 
