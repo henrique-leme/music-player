1f6b46f5e6475e07fdc2fc408088f14c8a519c1e
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-11-28 11:56:10 -0300

    ➕refactor: upodate submit

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -14,8 +14,8 @@ const Searchbar = () => {
 
   return (
     <form
+      onSubmit={handleSubmit}
       autoComplete="off"
-      action=""
       className="p-2 text-gray-400 focus-within:text-gray-600"
     >
       <label htmlFor="search-field" className="sr-only">Search for songs</label>
