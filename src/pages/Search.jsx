b9408e8731fabc584fc7387ba726af2b582e7ff8
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-11-28 12:00:13 -0300

    ➕refactor: add structure from topcharts

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,5 +1,34 @@
-const Search = () => (
-  <div>Search</div>
-);
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { Error, Loader, SongCard } from '../components';
+import { useGetSongsBySearchQuery } from '../redux/services/shazamCore';
+
+const Search = () => {
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data, isFetching, error } = useGetSongsBySearchQuery();
+
+  if (isFetching) return <Loader title="Loading songs around you..." />;
+  if (error) return <Error />;
+  return (
+    <div className="flex flex-col">
+      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
+        Discover Top Charts!!
+      </h2>
+      <div className="flex flex-wrap sm:justify-startjustify-center gap-8">
+        {data?.map((song, i) => (
+          <SongCard
+            key={song.key}
+            song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data}
+            i={i}
+          />
+        ))}
+      </div>
+    </div>
+
+  );
+};
 
 export default Search;
